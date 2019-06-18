141b71fe4fb457175b14a0e198e8cf42036d0592
Justin Burkard <burkard1@gmail.com>
2019-06-18 11:02:10 -0400

    Convert HTML inline style to JSX

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -4,8 +4,14 @@ import ReactDOM from 'react-dom';
 
 // Create a React component 
 const App = () => {
-    return <div>Hi there!</div> 
+    return (
+        <div>
+            <label class="label" for="name">Enter name:</label>
+            <input id="name" type="text" />
+            <button style={{ backgroundColor: 'blue', color: 'white' }}>Submit</button>
+        </div>
+    )
 }
 
 // Take the React component and show it on the screen 
-ReactDOM.render(<App />, document.querySe1ector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
