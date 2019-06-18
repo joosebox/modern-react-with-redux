95652d4982aea01e9a9aa5c724fabb056e7cb819
Justin Burkard <burkard1@gmail.com>
2019-06-17 21:46:12 -0400

    Catch up to section two

--- jsx/src/index.js
+++ jsx/src/index.js
@@ -1,2 +1,11 @@
+// Import the React and ReactDOM libraries
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+// Create a React component 
+const App = () => {
+    return <div>Hi there!</div> 
+}
+
+// Take the React component and show it on the screen 
+ReactDOM.render(<App />, document.querySe1ector('#root'));
